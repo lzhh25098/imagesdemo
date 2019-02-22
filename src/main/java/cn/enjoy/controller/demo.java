bfa7929916f8a2ac95f0b001eb5c03e127a04908
lzhh <2509687281@qq.com>
2019-02-22 18:42:28 +0800

    meven管理spring boot

--- src/main/java/cn/enjoy/controller/demo.java
+++ src/main/java/cn/enjoy/controller/demo.java
@@ -3,5 +3,6 @@ package cn.enjoy.controller;
 public class demo {
     public static void main(String[] args) {
         System.out.println("say hello");
+        System.out.println("test");
     }
 }
